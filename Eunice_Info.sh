dab44a7f8ffe6d7585850b59f53d6e457c45f5c3
euniceobeko <euniceobeko@gmail.com>
2024-08-06 06:01:41 +0100

    My Personal details

---
+++ Eunice_Info.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="Eunice Obeko"
+state="BenDel"
+LGA="Sapele"
+PrimarySchool="Triumphant Nursery and Primary School"
+SecondarySchool="Lords and Kings Academy"
+echo "Name: $name"
+echo "State: $state"
+echo "LGA: $LGA"
+echo "PrimarySchool: $PrimarySchool"
+echo "SecondarySchool: $SecondarySchool"
